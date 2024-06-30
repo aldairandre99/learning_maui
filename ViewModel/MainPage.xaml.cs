1e3bcaff45923ca9f3304bed1a33626ab0226e78
aldairandre99 <nzolageral@gmail.com>
2024-06-30 12:50:08 +0100

    Handle events without  data binding

---
+++ ViewModel/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		FirstName.TextChanged += FirstName_TextChanged;
+		LastName.TextChanged += LastName_TextChanged;
+
+	}
+
+	private void FirstName_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		UpdateLabel();
+	}
+
+	private void LastName_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		UpdateLabel();
+	}
+
+	void UpdateLabel()
+	{
+		FullName.Text = $"{FirstName.Text} {LastName.Text}";
+	}
+
+	private void ButtonCliked(object sender, EventArgs e)
+	{
+		JoinBtn.IsEnabled = false;
+
+		Console.WriteLine(FullName.Text);
+
+		JoinBtn.IsEnabled = true;
+	}
+
+}
+
