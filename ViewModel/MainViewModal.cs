beb5407296392aff522fe64fabac0e3b255d54f4
aldairandre99 <nzolageral@gmail.com>
2024-07-08 04:44:27 +0100

    Add the 4th operator

--- ViewModel/MainViewModal.cs
+++ ViewModel/MainViewModal.cs
@@ -15,17 +15,83 @@ public partial class MainViewModal : ObservableObject
   [ObservableProperty]
   int n2 = 0;
 
+  [ObservableProperty]
+  string mathOperator = "";
 
   [ObservableProperty]
-  double res = 0;
+  string res = "0";
 
 
 
   [RelayCommand]
-  void WriteInScreen(int txt)
+  void WriteInScreen(object p)
   {
-    Res = txt;
+
+    if (int.Parse(Res) == 0) Res = (string)p;
+    else Res += (string)p;
+
+    if (N1 < 0) N1 = int.Parse(Res);
+    else N2 = int.Parse(Res);
+  }
+  [RelayCommand]
+  void Operator(object op)
+  {
+    MathOperator = (string)op;
+    N1 = int.Parse(Res);
+    CleanScreen();
   }
 
+  [RelayCommand]
+  void CleanScreen()
+  {
+    Res = "0";
+  }
 
+  [RelayCommand]
+  void ShowResult(object p)
+  {
+    double op;
+    switch (MathOperator)
+    {
+      case "/":
+        if (N1 > 0 && N2 > 0)
+        {
+          op = N1 / N2;
+          N1 = 0;
+          N2 = 0;
+          Res = op.ToString();
+        }
+        break;
+      case "*":
+        if (N1 > 0 && N2 > 0)
+        {
+          op = N1 * N2;
+          N1 = 0;
+          N2 = 0;
+          Res = op.ToString();
+        }
+        break;
+      case "-":
+        if (N1 > 0 && N2 > 0)
+        {
+          op = N1 - N2;
+          N1 = 0;
+          N2 = 0;
+          Res = op.ToString();
+        }
+        break;
+      case "+":
+        if (N1 > 0 && N2 > 0)
+        {
+          op = N1 + N2;
+          N1 = 0;
+          N2 = 0;
+          Res = op.ToString();
+        }
+        break;
+      default:
+        break;
+    }
+
+  }
 }
